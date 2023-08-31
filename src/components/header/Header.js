302e6c8d36358b49c64201d7137ca9f0a184c163
Lulu Zhu <luluzhu9@gmail.com>
2023-08-31 10:47:15 -0700

    fixed prettier with write

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -51,38 +51,30 @@ const Header = () => {
       <div className="header-nav-wrapper">
         <div className="header-bar"></div>
         <div className="header-navbar">
-            <div className="header-image">
-                <img src={logo} alt=""/>
-            </div>
-            <div
-                className={`${menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'}`}
-                id="header-mobile-menu"
-                onClick={() => toggleMenu()}
-            >
-                <span className="bar"></span>
-                <span className="bar"></span>
-                <span className="bar"></span>
-            </div>
-            <ul className={`${navClass ? 'header-nav header-mobile-nav' : 'header-nav'}`}>
-                {
-                    HEADER_LIST.map((data) =>
-                    <li
-                        key={data.id}
-                        className="header-nav-item">
-                        <span className="header-list-name">
-                            <i className={data.iconClass}></i>
-                        </span>
-                        &nbsp;
-                        <span className="header-list-name">{data.name}</span>
-                    </li>
-                    )
-                }
-                <input
-                    className="search-input"
-                    type="text"
-                    placeholder="Search for a movie"
-                />
-            </ul>
+          <div className="header-image">
+            <img src={logo} alt="" />
+          </div>
+          <div
+            className={`${menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'}`}
+            id="header-mobile-menu"
+            onClick={() => toggleMenu()}
+          >
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+          <ul className={`${navClass ? 'header-nav header-mobile-nav' : 'header-nav'}`}>
+            {HEADER_LIST.map((data) => (
+              <li key={data.id} className="header-nav-item">
+                <span className="header-list-name">
+                  <i className={data.iconClass}></i>
+                </span>
+                &nbsp;
+                <span className="header-list-name">{data.name}</span>
+              </li>
+            ))}
+            <input className="search-input" type="text" placeholder="Search for a movie" />
+          </ul>
         </div>
       </div>
     </>

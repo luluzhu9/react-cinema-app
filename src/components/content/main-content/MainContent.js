de0cbdbc983ef2fe4257e3d0de864143dac6b43c
Lulu Zhu <luluzhu9@gmail.com>
2023-09-13 14:26:50 -0700

    created api service and redux actions

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,59 +1,97 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import './MainContent.scss';
 import Slideshow from '../slide-show/Slideshow';
 import Paginate from '../paginate/Paginate';
 import Grid from '../grid/Grid';
+import { IMAGE_URL } from '../../../services/movies.service';
+import { getMovies, setResponsePageNumber } from '../../../redux/actions/movies';
 
-const MainContent = () => {
-  const images = [
-    {
-      url: 'https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?cs=srgb&dl=pexels-pixabay-33129.jpg&fm=jpg',
-      rating: 7.5
-    },
-    {
-      url: 'https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?cs=srgb&dl=pexels-pixabay-33129.jpg&fm=jpg',
-      rating: 8.5
-    },
-    {
-      url: 'https://cdn.pixabay.com/photo/2018/01/03/01/17/film-3057394_640.jpg',
-      rating: 7.8
-    },
-    {
-      url: 'https://cdn.pixabay.com/photo/2018/01/03/01/17/film-3057394_640.jpg',
-      rating: 9.7
-    },
-    {
-      url: 'https://cdn.pixabay.com/photo/2018/01/03/01/17/film-3057394_640.jpg',
-      rating: 6.5
-    },
-    {
-      url: 'https://image.cnbcfm.com/api/v1/image/107112713-1662049046824-gettyimages-1146819830-ai7i1036copy.jpeg?v=1662049153',
-      rating: 8.5
+const MainContent = (props) => {
+  const { list, movieType, totalPages, page, getMovies, setResponsePageNumber } = props;
+  const [currentPage, setCurrentPage] = useState(page);
+  const [images, setImages] = useState([]);
+  const randomMovies = list.sort(() => Math.random() - Math.random()).slice(0, 4);
+
+  const HEADER_TYPE = {
+    now_playing: 'Now Playing',
+    popular: 'Popular',
+    top_rated: 'Top Rated',
+    upcoming: 'Upcoming'
+  };
+
+  useEffect(() => {
+    if (randomMovies.length) {
+      const IMAGES = [
+        {
+          id: 1,
+          url: `${IMAGE_URL}${randomMovies[0].backdrop_path}`
+        },
+        {
+          id: 2,
+          url: `${IMAGE_URL}${randomMovies[1].backdrop_path}`
+        },
+        {
+          id: 3,
+          url: `${IMAGE_URL}${randomMovies[2].backdrop_path}`
+        },
+        {
+          id: 4,
+          url: `${IMAGE_URL}${randomMovies[3].backdrop_path}`
+        }
+      ];
+      setImages(IMAGES);
     }
-  ];
-  const [currentPage, setCurrentPage] = useState(1);
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    setCurrentPage(page);
+    // eslint-disable-next-line
+  }, [page, totalPages]);
 
   const paginate = (type) => {
-    if (type === 'prev' && currentPage > 1) {
-      setCurrentPage((prev) => prev - 1);
+    let pageNumber = currentPage;
+    if (type === 'prev' && currentPage >= 1) {
+      pageNumber -= 1;
     } else {
-      setCurrentPage((prev) => prev + 1);
+      pageNumber += 1;
     }
+    setCurrentPage(pageNumber);
+    setResponsePageNumber(pageNumber, totalPages);
+    getMovies(movieType, pageNumber);
   };
 
   return (
     <div className="main-content">
       <Slideshow images={images} auto={true} showArrows={true} />
       <div className="grid-movie-title">
-        <div className="movieType">Now Playing</div>
+        <div className="movieType">{HEADER_TYPE[movieType]}</div>
         <div className="paginate">
-          <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
+          <Paginate currentPage={currentPage} totalPages={totalPages} paginate={paginate} />
         </div>
       </div>
-      <Grid images={images} />
+      <Grid />
     </div>
   );
 };
 
-export default MainContent;
+MainContent.propTypes = {
+  list: PropTypes.array.isRequired,
+  movieType: PropTypes.string.isRequired,
+  totalPages: PropTypes.number.isRequired,
+  page: PropTypes.number.isRequired,
+  getMovies: PropTypes.func.isRequired,
+  setResponsePageNumber: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  list: state.movies.list,
+  movieType: state.movies.movieType,
+  totalPages: state.movies.totalPages,
+  page: state.movies.page
+});
+
+export default connect(mapStateToProps, { getMovies, setResponsePageNumber })(MainContent);
